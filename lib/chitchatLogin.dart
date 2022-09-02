b88d33e43e67b96cc336d91749cc30815c50d486
Anil Raj <anilrajpallana@gmail.com>
2022-09-02 23:05:39 +0530

    Trying something new

---
+++ lib/chitchatLogin.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+
+class Login extends StatelessWidget {  @override
+  Widget build(BuildContext context) {
+  return Scaffold(backgroundColor: Colors.white,
+    appBar: AppBar(title: Text("Login Page",style: TextStyle(color: Colors.white70,fontSize: 20,fontWeight: FontWeight.bold),),
+
+   shadowColor: Colors.black45, ),
+
+  );
+  }
+}
--- lib/splashset.dart
+++ lib/splashset.dart
@@ -1,23 +1,45 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main(){
-  runApp(Splahier());
+import 'package:flutter/material.dart';
+import 'chitchatLogin.dart';
+
+void main() {
+  runApp( MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.lightGreen),
+    debugShowCheckedModeBanner: false,home: Splahier(),
+  ));
 }
 
-class Splahier extends StatelessWidget {
+class Splahier extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp( debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: Scaffold(
-        body: Column( mainAxisAlignment: MainAxisAlignment.center,children: [
-          Text("CHIT\nCHAt",style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),)
-        ],
-        ),
-      ),
+  State<Splahier> createState() => _SplahierState();
+}
 
-
-    );
+class _SplahierState extends State<Splahier> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Login()));
+    });
   }
 
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Image(
+                height: 350,
+                width: 350,
+                image: AssetImage(
+                    "assets/icons/CHIT CHAT-logos_transparent.png"))
+          ],
+        ),
+      ),
+    );
+  }
 }
