3f89d0ed86915427fbb502558f0a321aaf2ba23b
Anil Raj <anilrajpallana@gmail.com>
2022-09-02 23:06:30 +0530

    Trying something new

--- lib/chitchatLogin.dart
+++ lib/chitchatLogin.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {  @override
   Widget build(BuildContext context) {
   return Scaffold(backgroundColor: Colors.white,
-    appBar: AppBar(title: Text("Login Page",style: TextStyle(color: Colors.white70,fontSize: 20,fontWeight: FontWeight.bold),),
+    appBar: AppBar(title: Text("Login Page",style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
 
    shadowColor: Colors.black45, ),
 
